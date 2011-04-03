8a2dd2714fdc73af031a7c588cc63e2ac784d00a
huallpa.nestor <huallpa.nestor@42c75038-8da6-4cae-7175-a1a39666e3c5>
2011-04-03 15:09:41 +0000

    creando lista de instrucciones

---
+++ trunk/testListaRepetir.h
@@ -0,0 +1,22 @@
+/*
+ * testListaRepetir.h
+ *
+ *  Created on: 03/04/2011
+ *      Author: Nestor Huallpa
+ */
+#ifndef __TESTLISTAREPETIR_H_
+#define __TESTLISTAREPETIR_H_
+
+#include "listaInstrucciones.h"
+#include "instruccion.h"
+#include "malloc.h"
+
+
+void mainTestSuiteLista();
+
+void testReservarYLiberar();
+
+void testAgregarYRecuperar();
+
+
+#endif /* TESTLISTAREPETIR_H_ */
